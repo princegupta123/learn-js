9bea9a2cfbee49fb506549c3b45a933b60d347d9
PrinceGupta123 <101912699+PrinceGupta123@users.noreply.github.com>
2023-12-25 20:12:39 +0530

    push

---
+++ 05_iteration/four.js
@@ -0,0 +1,28 @@
+//for in loop can iterate on object and return its key but for of loop does not and forin loop don't work on map, also in array it return key means index
+//
+const myObject = {
+  js: "javascript",
+  cpp: "C++",
+  rb: "ruby",
+  swift: "swift by apple",
+};
+
+for (const key in myObject) {
+  //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const key in programming) {
+  //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
