49c9a83e98867aa750480dfa84d75e2846c14dfb
Prince Gupta <prince.gupta@almond.solutions>
2023-12-26 17:42:21 +0530

    push

---
+++ 07_promises/one.js
@@ -0,0 +1,13 @@
+ //async
+
+
+ //js is sync and single thread language
+
+
+ //code executes line by line
+
+
+ //blocking vs non-blocking
+
+ // block all execution
+ //run in parallel
---
+++ 07_promises/project_async.js
@@ -0,0 +1,12 @@
+// const timeOut = setTimeout(()=>{
+//     console.log("I am the greatest")
+// }, 3000);
+
+// clearTimeout(timeOut);
+
+
+const interval = setInterval(()=>{
+    console.log("Hi, Prince")
+}, 1000);
+
+clearInterval(interval)
